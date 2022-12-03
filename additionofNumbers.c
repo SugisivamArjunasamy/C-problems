0cb1408a4f447d24d9cffec064e5d29aee5883fb
SugisivamArjunasamy <sugisivamarjunasamy2004@gmail.com>
2022-12-03 22:36:52 +0530

    3 dec 2022

---
+++ additionofNumbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int num1,num2;
+    printf("Enter tha numbers");
+    scanf("%d %d",&num1,&num2);
+    printf("The addition of two numbers is : %d",(num1+num2));
+
+
+    return 0;
+}
