da9bf5b6a7cf0c0f25a9a079f3568bbb91ca1791
SugisivamArjunasamy <sugisivamarjunasamy2004@gmail.com>
2022-12-04 20:43:43 +0530

    4 dec 2022

---
+++ greatestofTwoNumbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int num1,num2;
+    printf("Enter the numbers : ");
+    scanf("%d %d",&num1,&num2);
+    if(num1>num2)
+     printf("Number1 is greater");
+    else 
+     printf("Number2 is greater");
+
+
+    return 0;  
+}
