8a711f2de5681846428ab77dab7cede30bb2b859
SugisivamArjunasamy <sugisivamarjunasamy2004@gmail.com>
2022-12-06 20:35:02 +0530

    6 dec 2022

---
+++ oddorEven.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main (){
+    int num1;
+    printf("Enter the number : ");
+    scanf("%d",&num1);
+    if(num1%2==0)
+     printf("the Number is even");
+    else 
+     printf("the number is odd");
+
+
+    return 0;
+    
+}
