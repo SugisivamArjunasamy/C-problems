f5a8a915af1a5ac377c33d8ab7a9c572c6f5fda7
SugisivamArjunasamy <sugisivamarjunasamy2004@gmail.com>
2022-12-07 20:40:35 +0530

    7 dec 2022

---
+++ multiplication.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int num1,num2,multiplication;
+    printf("enter the numbers : ");
+    scanf("%d%d",&num1,&num2);
+    multiplication=num1*num2;
+    printf("\nthe multiplication is : %d",multiplication);
+    return 0;
+}
