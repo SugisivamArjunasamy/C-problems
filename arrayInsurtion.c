809dbc343ce11bf7f5107ada7e93f8ee7ec8809c
Sugisivam Arjunasamy <sugisivamarjunasmy2004@gmail.com>
2023-04-11 17:41:45 +0530

    11.04.2023

---
+++ arrayInsurtion.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int array[100];
+    int n,poss=3;
+    printf("Enter the elements off array");
+    scanf("%d",&n);
+    printf("Enter the array elements\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&array[i]);
+    }
+    for(int i=n;i>=poss;i--)
+    {
+        array[i]=array[i-1];
+        array[2]=25;
+    }
+
+
+    return 0;
+    
+
+}
