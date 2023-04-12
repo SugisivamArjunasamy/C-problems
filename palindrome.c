cc2e8c5b6e6b2fb0762287c8a1c87cdffe1def0f
Sugisivam Arjunasamy <sugisivamarjunasmy2004@gmail.com>
2023-04-12 21:51:51 +0530

    12.04.2023

---
+++ palindrome.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,rev=0,remin;
+    printf("Enter an integer");
+    scanf("%d",&num1);
+    num2=num1;
+
+    while(num1!=0)
+    {
+        remin=num1%10;
+        rev=rev*10+remin;
+        num1/=10;
+    }
+    if(num2==rev)
+        printf("%d is a polindrome number",num2);
+    else
+        printf("%d is a not an polindrome number",num2);
+    
+
+    return 0;
+}
