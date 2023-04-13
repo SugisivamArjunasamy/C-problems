9f0b84c8a2376a8afaea17b5ee070ff4a5a248d0
Sugisivam Arjunasamy <sugisivamarjunasmy2004@gmail.com>
2023-04-13 18:11:54 +0530

    13.03.23

---
+++ asciivalueofCharecter.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int char a;
+    printf("Enter an charecter : ");
+    scanf("%c",&a);
+
+    //prinnting the ASCII value of the charecter
+    printf("The ASCII value of %c=%d",a,a);
+
+    return 0;
+}
