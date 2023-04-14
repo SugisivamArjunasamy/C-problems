a94151a7b5913c59079b4cafe3f40aa757cbbb13
Sugisivam Arjunasamy <sugisivamarjunasmy2004@gmail.com>
2023-04-14 18:18:05 +0530

    14.04.23

---
+++ swapUsingPointer.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int swap(int a,int b);
+int main()
+{
+    int a,b;
+    printf("Enter two numbers : ");
+    scanf("%d%d",&a,&b);
+    printf("Before swap a=%d , b=%d",a,b);
+    swap(&a,&b);
+    return 0;
+}
+int swap(int *a,int *b)
+{
+    int c=*a;
+    *a=*b;
+    *b=c;
+    printf("After swap a=%d , b=%d",a,b);
+}
